a7b722d1d97bae1195233f0d87208226afdcf8f9
pe42sha <43850160+pe42sha@users.noreply.github.com>
2018-10-04 14:56:45 +0100

    updated Task 3

--- lab_101/3_exercises/src/a_traffic_light.c
+++ lab_101/3_exercises/src/a_traffic_light.c
@@ -14,10 +14,42 @@
 #include "gpio.h"
 
 // map the led to GPIO PA8
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led1 = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led2 = {PB_14, GPIOI, GPIO_PIN_1};
+gpio_pin_t led3 = {PB_15, GPIOI, GPIO_PIN_1};
 
 // this is the main method
 int main()
 {
-
+  // we need to initialise the hal library and set up the SystemCoreClock 
+  // properly
+  HAL_Init();
+  init_sysclk_216MHz();
+	
+	// initialise the gpio pins
+  init_gpio(led1, OUTPUT);
+	init_gpio(led2, OUTPUT);
+	init_gpio(led3, OUTPUT);
+	
+	// loop forever ...
+  while(1)
+  {
+    // toggl
+    toggle_gpio(led1);
+		
+		// wait for 1 second
+    HAL_Delay(1000);
+		
+		toggle_gpio(led2);
+		
+		// wait for 1 second
+    HAL_Delay(1000);
+		
+		toggle_gpio(led1);
+		toggle_gpio(led2);
+		toggle_gpio(led3);
+    
+    // wait for 1 second
+    HAL_Delay(1000);
+  }
 }
