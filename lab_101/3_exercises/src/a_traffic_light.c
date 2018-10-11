7525aaffb658934e30d8ce4676a3b800b56974f0
pe42sha <43850160+pe42sha@users.noreply.github.com>
2018-10-11 14:41:12 +0100

    broke a lot of stuff

--- lab_101/3_exercises/src/a_traffic_light.c
+++ lab_101/3_exercises/src/a_traffic_light.c
@@ -14,9 +14,9 @@
 #include "gpio.h"
 
 // map the led to GPIO PA8
-gpio_pin_t led1 = {PI_1, GPIOI, GPIO_PIN_1};
-gpio_pin_t led2 = {PB_14, GPIOI, GPIO_PIN_1};
-gpio_pin_t led3 = {PB_15, GPIOI, GPIO_PIN_1};
+gpio_pin_t led1 = {PI_1, GPIOI, GPIO_PIN_1}; //red traffic light
+gpio_pin_t led2 = {PB_14, GPIOI, GPIO_PIN_14};//mber traffic light
+gpio_pin_t led3 = {PB_15, GPIOI, GPIO_PIN_15}; //een traffic light
 
 // this is the main method
 int main()
@@ -34,22 +34,33 @@ int main()
 	// loop forever ...
   while(1)
   {
-    // toggl
-    toggle_gpio(led1);
+    // red on
+    write_gpio(led1, HIGH);
+		write_gpio(led2, LOW);
+		write_gpio(led3, LOW);
 		
 		// wait for 1 second
     HAL_Delay(1000);
 		
-		toggle_gpio(led2);
+		//red & amber on
+		write_gpio(led2, HIGH);
 		
 		// wait for 1 second
     HAL_Delay(1000);
 		
-		toggle_gpio(led1);
-		toggle_gpio(led2);
-		toggle_gpio(led3);
+		// green on
+		write_gpio(led1, LOW);
+		write_gpio(led2, LOW);
+		write_gpio(led3, HIGH);
     
-    // wait for 1 second
-    HAL_Delay(1000);
+    // wait for 5 seconds
+    HAL_Delay(5000);
+		
+		// amber on
+		write_gpio(led2, HIGH);
+		write_gpio(led3, LOW);
+		
+		///wait for one second
+		HAL_Delay(1000);
   }
 }
